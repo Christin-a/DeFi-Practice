08a0512247864e99cac0f8e18a0e79712a1ca70c
Christina Armand <christina@Christinas-MacBook-Air.local>
2022-04-27 01:02:45 -0400

    first commit

--- src/dbank/main.mo
+++ src/dbank/main.mo
@@ -1,5 +1,48 @@
-actor {
-  public func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+import Debug "mo:base/Debug"; 
+import Time "mo:base/Time"; 
+import Float "mo:base/Float"; 
+
+
+
+actor DBank { 
+  stable var currentValue:Float = 300; //"stable" turns variable from being flexible to being persistent 
+  currentValue:= 300; 
+  let id = 3234234; 
+  Debug.print("Hello"); 
+
+  stable var startTime = Time.now();
+  Debug.print(debug_show(startTime));
+
+
+  public func topUp(amount:Float) { ///creates a  public function with input 'amount' originally in the form of a natural number(Nat) 
+  // changed from Nat to Float to match data type of currentValue
+  //without the 'public' keyword, the function would be private and could only be called within the DBank actor
+    currentValue += amount;
+    Debug.print(debug_show(currentValue));
   };
-};
+
+  public func withdraw(amount: Float){
+    let tempValue: Float = currentValue - amount; // sets tempValue to be Floating interger
+    // tempValue was originally an Int (interger)
+    if (tempValue>=0) { // if statement that notifies the developer if currentValue becomes less than zero
+    currentValue -= amount;
+    Debug.print(debug_show(currentValue));
+    }else{
+        Debug.print("Amount too large, currentValue less than zero.")
+    }
+  };
+
+    public query func checkBalance(): async Float { // creates an asynchonous read-only function 
+      return currentValue
+    };
+
+  public func compound(){ //calculate compound interest of 1% every second
+    let currentTime = Time.now();
+    let timeElapsedS = (currentTime-startTime)/1000000000; // converts time from nanoseconds to seconds
+    currentValue := currentValue * (1.01 ** Float.fromInt(timeElapsedS)); //changes the value of currentTime to reflect the compound equation
+    //float turns time elapse into a floating interger so that it's the same data type as currentValue
+    startTime := currentTime;
+  } 
+
+}
+   
--- src/dbank_assets/src/index.js
+++ src/dbank_assets/src/index.js
@@ -1,19 +1,41 @@
-import { dbank } from "../../declarations/dbank";
+import { dbank } from "../../declarations/dbank"; // exposes motoko code to Javascript
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+window.addEventListener("load", async function() { // once the page loads, the window executes the update function asynchronously
+  // console.log("Finished loading");
+  update();
+});
 
-  const name = document.getElementById("name").value.toString();
+document.querySelector("form").addEventListener("submit", async function(event) {//creates a function when the form is submitted
+  event.preventDefault(); // keeps form from reloading
+  // console.log("Submitted.");
+
+  const button = event.target.querySelector("#submit-btn");
+
+  const inputAmount = parseFloat(document.getElementById("input-amount").value); //stores the user input amount in the input amount and parses it into a floating point number
+  const outputAmount = parseFloat(document.getElementById("withdrawal-amount").value); //stores the user withdraw amount in the input amount
 
   button.setAttribute("disabled", true);
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await dbank.greet(name);
+  if (document.getElementById("input-amount").value.length != 0) { //only tops up when user types into the top up input area
+    await dbank.topUp(inputAmount);
+  }
 
-  button.removeAttribute("disabled");
+  if (document.getElementById("withdrawal-amount").value.length != 0) { //only withdraws when user types into the top up input area
+    await dbank.withdraw(outputAmount);
+  }
 
-  document.getElementById("greeting").innerText = greeting;
+  await dbank.compound();
 
-  return false;
+  update()
+
+  document.getElementById("input-amount").value = "";
+  document.getElementById("withdrawal-amount").value = "";
+
+  button.removeAttribute("disabled");  //disables button after user hits submit
+
 });
+
+async function update() {
+  const currentAmount = await dbank.checkBalance(); // wait for the the checkBalance function
+  document.getElementById("value").innerText = Math.round(currentAmount * 100) / 100;
+};
